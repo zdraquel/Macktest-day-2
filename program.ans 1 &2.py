e0cd9c88e6e63a2ec659a413868da104a610b077
zdraquel <143689837+zdraquel@users.noreply.github.com>
2024-05-08 14:17:06 +0530

    Add files via upload

---
+++ program.ans 1 &2.py
@@ -0,0 +1,16 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/DIVYAA/AppData/Local/Programs/Python/Python312/python.exe2.py
+Usecase 1:
+3
+Use case 2:
+58
+Use case 3:
+1994
+>>> 
+= RESTART: C:/Users/DIVYAA/AppData/Local/Programs/Python/Python312/python.exe1.py
+Usecase 1:
+fl
+Use case 2:
+
---
+++ python.prgm1.py
@@ -0,0 +1,16 @@
+def solution(strs: list):
+    a=strs[0]
+    for i in range(1,len(strs)):
+        while strs[i].find(a)!=0:
+            a=a[:-1]
+    return a
+
+# Use case 1
+print("Usecase 1:")
+strs=["flowers","flow","flight"]
+print(solution(strs))
+
+#Use case 2:
+print("Use case 2:")
+strs2=["dog","racecar","car"]
+print(solution(strs2))
